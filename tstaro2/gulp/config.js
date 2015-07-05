0ed8499c1aaab49957f2cee5db3fc2b5566e7257
Koji Suda <sudako2006@gmail.com>
2015-07-06 01:33:33 +0900

    環境整備（ひとまずいい感じ）

--- tstaro2/gulp/config.js
+++ tstaro2/gulp/config.js
@@ -1,5 +1,8 @@
-﻿var dest = './public';
+﻿var path = require('path');
+
+var dest = './public';
 var src = './src';
+var relativeSrcPath = path.relative('.', src);
 
 module.exports = {
     dest: dest,
@@ -15,8 +18,31 @@ module.exports = {
         output: {
             filename: 'app.js'
         },
-        resolv: {
+        devtool: 'inline-source-map',
+        module: {
+            loaders: [
+                { test: /\.jsx$/, loader: 'jsx-loader' }
+            ]
+        },
+        resolve: {
             extensionx: [ '', 'js', 'jsx' ]
         }
+    },
+
+    stylus: {
+        src: [  // もし外部のcssフレームワーク使うなら配列の先頭で読み込むと良い
+            src + '/styl/**/!(_)*'  // ファイル名の先頭がアンスコはビルド対象外にする
+        ],
+        dest: dest + '/stylesheets',
+        output: 'style.css',  // 出力ファイル名
+        autoprefixer: {
+            browsers: ['last 2 versions']
+        },
+        minify: false
+    },
+
+    watch: {
+        js: relativeSrcPath + '/jsx/**',
+        styl: relativeSrcPath + '/styl/**'
     }
 };
---
+++ tstaro2/gulp/tasks/watch.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp');
+var watch = require('gulp-watch');
+var config = require('../config').watch;
+
+gulp.task('watch', function () {
+    // js
+    watch(config.js, function () {
+        gulp.start(['webpack']);
+    });
+    
+    // styl
+    watch(config.styl, function () {
+        gulp.start(['stylus']);
+    });
+});
