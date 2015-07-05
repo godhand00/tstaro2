be68ab26461b1159ed13a4c8e35639e633a7b3df
Koji Suda <sudako2006@gmail.com>
2015-07-06 01:00:12 +0900

    a

---
+++ tstaro2/gulp/config.js
@@ -0,0 +1,22 @@
+﻿var dest = './public';
+var src = './src';
+
+module.exports = {
+    dest: dest,
+
+    js: {
+        src: src + '/jsx/**',
+        dest: dest + '/javascripts',
+        uglify: false   
+    },
+
+    webpack: {
+        entry: src + '/jsx/app.jsx',
+        output: {
+            filename: 'app.js'
+        },
+        resolv: {
+            extensionx: [ '', 'js', 'jsx' ]
+        }
+    }
+};
--- tstaro2/gulpfile.js
+++ tstaro2/gulpfile.js
@@ -1,14 +1,2 @@
-﻿var gulp = require('gulp');
-var webpack = require('gulp-webpack');
-var webpackConfig = require('./webpack.config.js');
-
-gulp.task('cleanBuild', function (cb) {
-    var rimraf = require('rimraf');
-    rimraf('./public/javascripts/', cb);
-});
-
-gulp.task('build', function (cb) {
-    return gulp.src('')
-    .pipe(webpack(webpackConfig))
-    .pipe(gulp.dest(''));
-});
+﻿var requireDir = require('require-dir');
+requireDir('./gulp/tasks', { recurse: true });
